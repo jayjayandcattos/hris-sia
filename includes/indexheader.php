a1416dfeed411fa8d2e135dbecb4114610d4a6a0
Justin Rivera <theresumedbelt@gmail.com>
2025-10-25 10:10:02 +0800

    navbar-responsive

--- includes/indexheader.php
+++ includes/indexheader.php
@@ -1,5 +1,6 @@
 <head>
   <title>ELECT</title>
+  <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="css/header.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Saira:wght@600&display=swap" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
@@ -10,20 +11,19 @@
 <body>
   <div class="header">
     <div class="navbar">
-      <div class="nav-content">
-        <div class="nav-left">
-          <div class="title">
-            <a href="user/logout.php" title="Logout">
-              <img class="logo-nav dark" src="assets/LOGO.png" alt="ELECT LOGO">
+      <div class="nav-content flex flex-col md:flex-row items-center justify-between gap-3 md:gap-0 px-4 sm:px-6">
+        <div class="nav-left flex items-center w-full md:w-auto justify-center md:justify-start">
+          <div class="title flex items-center gap-3">
+            <a href="user/logout.php" title="Logout" class="hidden md:block">
+              <img class="logo-nav dark w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14" src="assets/LOGO.png" alt="ELECT LOGO">
             </a>
-            <h1 style="font-size: 30px; padding: 5px;">EVERGREEN</h1>
+            <h1 class="text-2xl sm:text-3xl" style="padding: 5px;">EVERGREEN</h1>
           </div>
-
         </div>
 
-        <div class="nav-right">
+        <div class="nav-right !hidden md:!flex items-center w-full md:w-auto justify-center md:justify-end">
           <div class="date">
-            <div class="date1" id="currentDate">Date</div>
+            <div class="date1 text-sm sm:text-base md:text-lg" id="currentDate">Date</div>
           </div>
 
           <!-- <div class="time">
